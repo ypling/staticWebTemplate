d33f0c16d24ca25670b327c9355c963b5cf525f1
lhz213 <lhz_work@hotmail.com>
2015-04-29 00:56:01 -0700

    layout basically done

--- javascript/main.js
+++ javascript/main.js
@@ -9,35 +9,37 @@
     var movingBGImgs;
     //function define
     function moveBackgroundImg(scrollOffset) { //move background image base on window scroll offset.
-        movingBGImgs.each(function(){
+        movingBGImgs.each(function () {
             var element = $(this);
-            var offset = element.offset().top -navigationBarHeight - scrollOffset /1.5;
+            var offset = element.offset().top - navigationBarHeight - scrollOffset / 1.5;
             element.css('background-position', '50% ' + offset + "px");
         });
     }
 
-    function lineWrapAnimation(containerId,elementClass,gap) {
+    function lineWrapAnimation(containerId, elementClass, gap) {
         var elementWidth = $(elementClass).width();
         var elementHeight = $(elementClass).height();
-        var numberForLine = Math.floor($(containerId).width()/elementWidth);
-        var max = 0 ;
-        $(elementClass+':visible').each(function (index) {
-            var numberOfLine = Math.floor(index / numberForLine)+1;
-            if(numberOfLine>max){
+        var numberForLine = Math.floor($(containerId).width() / elementWidth);
+        var max = 0;
+        $(elementClass + ':visible').each(function (index) {
+            var numberOfLine = Math.floor(index / numberForLine) + 1;
+            if (numberOfLine > max) {
                 max = numberOfLine;
             }
             $(this).animate({
-                left:index % numberForLine * elementWidth + gap,
-                top:Math.floor(index / numberForLine) * elementHeight + gap
+                left: index % numberForLine * elementWidth + gap,
+                top: Math.floor(index / numberForLine) * elementHeight + gap
             })
         });
-        $(elementClass+':hidden').each(function (index) {
+        $(elementClass + ':hidden').each(function (index) {
             $(this).css(
-                {left:gap,
-                top:gap}
+                {
+                    left: gap,
+                    top: gap
+                }
             )
         });
-        $(containerId).animate({height:(elementHeight+gap)*max});
+        $(containerId).animate({height: elementHeight * max + gap});
     }
 
     $document.ready(function () {
@@ -64,15 +66,16 @@
             moveBackgroundImg($document.scrollTop());
         });
 
-        $(window).resize(function(){
-            lineWrapAnimation('#empContainer', '.emp-show', 80);
+        $(window).resize(function () {
+            lineWrapAnimation('.emp-container', '.emp-show', 80);
+            lineWrapAnimation('.stu-container', '.stu-show', 80);
         });
 
         // faq controller
         $('.panel-body').hide();
-        $('.panel-heading').on('click',function(){
+        $('.panel-heading').on('click', function () {
             var answer = $(this).siblings();
-            if ( answer.is( ":hidden" ) ) {
+            if (answer.is(":hidden")) {
                 answer.slideDown();
             } else {
                 answer.slideUp();
@@ -81,38 +84,51 @@
         });
 
         //init employee positions
-        lineWrapAnimation('#empContainer','.emp-show',80);
+        lineWrapAnimation('.emp-container', '.emp-show', 80);
+        lineWrapAnimation('.stu-container', '.stu-show', 80);
 
         //contact form validation
         var search_str = /^[\w\-\.]+@[\w\-\.]+(\.\w+)+$/;
-        $("#errorName").css("display","none");
-        $("#errorEmailApply").css("display","none");
-        $("#errorEmailContact").css("display","none");
-        $("#contactName").focus(function(){
-            $(this).css("background-color","#FFFFCC");
+        $("#errorName").css("display", "none");
+        $("#errorEmailApply").css("display", "none");
+        $("#errorEmailContact").css("display", "none");
+        $("#contactName").focus(function () {
+            $(this).css("background-color", "#FFFFCC");
         });
-        $("#contactName").blur(function(){
-            $(this).css("background-color","#D6D6FF");
-            if($(this).val().length<4){$("#errorName").css("display","block");}
-            else{$("#errorName").css("display","none");}
+        $("#contactName").blur(function () {
+            $(this).css("background-color", "#D6D6FF");
+            if ($(this).val().length < 4) {
+                $("#errorName").css("display", "block");
+            }
+            else {
+                $("#errorName").css("display", "none");
+            }
         });
-        $("#contactEmailApply").focus(function(){
-            $(this).css("background-color","#FFFFCC");
+        $("#contactEmailApply").focus(function () {
+            $(this).css("background-color", "#FFFFCC");
         });
-        $("#contactEmailApply").blur(function(){
-            $(this).css("background-color","#D6D6FF");
+        $("#contactEmailApply").blur(function () {
+            $(this).css("background-color", "#D6D6FF");
             var email_val = $("#contactEmailApply").val();
-            if(!search_str.test(email_val)){$("#errorEmailApply").css("display","block");}
-            else{$("#errorEmailApply").css("display","none");}
+            if (!search_str.test(email_val)) {
+                $("#errorEmailApply").css("display", "block");
+            }
+            else {
+                $("#errorEmailApply").css("display", "none");
+            }
         });
-        $("#contactEmailContact").focus(function(){
-            $(this).css("background-color","#FFFFCC");
+        $("#contactEmailContact").focus(function () {
+            $(this).css("background-color", "#FFFFCC");
         });
-        $("#contactEmailContact").blur(function(){
-            $(this).css("background-color","#D6D6FF");
+        $("#contactEmailContact").blur(function () {
+            $(this).css("background-color", "#D6D6FF");
             var email_val = $(this).val();
-            if(!search_str.test(email_val)){$("#errorEmailContact").css("display","block");}
-            else{$("#errorEmailContact").css("display","none");}
+            if (!search_str.test(email_val)) {
+                $("#errorEmailContact").css("display", "block");
+            }
+            else {
+                $("#errorEmailContact").css("display", "none");
+            }
         });
         //init tooltips
         $('[data-toggle="tooltip"]').tooltip();
@@ -131,7 +147,7 @@
 
         var defaults = {
                 elem: $(this),
-                speed: 500
+                speed: 1000
             },
 
             allOptions = $.extend(defaults, options);
@@ -160,26 +176,46 @@
 
     $.fn.employeeFilter = function () {
         var filterArea = $(this).closest('section');
-        var employeeCell = filterArea.find('div.emp');
+        var employeeCell = filterArea.find('div.emp-show');
         $(this).on('click', function () {
             var $this = $(this);
             var needShowClass = $this.attr("class") ? $this.attr("class") : "all";
             if (needShowClass !== "all") {
-                employeeCell.parent().hide();
-                filterArea.find("div." + needShowClass).show();
+                employeeCell.hide();
+                filterArea.find("div." + needShowClass).show()
+                lineWrapAnimation('.emp-container', '.emp-show', 80);
             } else {
-                employeeCell.parent().show();
+                employeeCell.show();
+                lineWrapAnimation('.emp-container', '.emp-show', 80);
             }
         });
-        lineWrapAnimation('#empContainer', '.emp-show', 80);
-        
-        employeeCell.on('mouseenter',function(){
+
+        employeeCell.on('mouseenter', function () {
 
         });
-        employeeCell.on('mouseleave',function(){
+        employeeCell.on('mouseleave', function () {
 
         });
     };
 
 })(jQuery, 'smartresize');
 
+//
+//var tag = document.createElement('script');
+//tag.src = "http://www.youtube.com/player_api";
+//var firstScriptTag = document.getElementsByTagName('script')[0];
+//firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//
+//var player;
+//function onYouTubePlayerAPIReady() {
+//    player = new YT.Player('player', {
+//        playerVars: { 'autoplay': 1, 'controls': 1,'autohide':1,'wmode':'opaque' },
+//        videoId: 'gdqvRBO0_ek',
+//        events: {
+//            'onReady': onPlayerReady}
+//    });
+//}
+//
+//function onPlayerReady(event) {
+//    event.target.mute();
+//}
